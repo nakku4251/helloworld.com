551ac30d9cb3f3b227573d5a2c6279290b85e3d8
柳達也 <nakku@nakkunoMacBook-Air.local>
2021-02-06 22:34:20 +0900

    commit

--- reviews/views.py
+++ reviews/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from  .models import Review
 from django.shortcuts import render, get_object_or_404, redirect
+from .forms import ReviewCreateForm
 # Create your views here.
 def review_list(request):
     context = {
@@ -15,9 +16,35 @@ def review_detail(request, pk):
     return render(request, "reviews/review_detail.html", context)
 
 def review_create(request):
-    return render(request, "reviews/review_form.html")
+    form = ReviewCreateForm(request.POST or None)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect("reviews:review_list")
 
-def review_create_send(request):
-    name = request.POST.get("store_name")
-    print("返信されたデータ→{}".format(name))
-    return redirect("reviews:review_list")
+    context = {
+        "form": form
+    }
+    return render(request, "reviews/review_form.html" , context)
+
+def review_update(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    form = ReviewCreateForm(request.POST or None, instance=review)
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect("reviews:review_list")
+
+    context = {
+        "form": form
+    }
+    return render(request, "reviews/review_form.html", context)
+
+def review_delete(request, pk):
+    review = get_object_or_404(Review, pk=pk)
+    if request.method == "POST":
+        review.delete()
+        return redirect("reviews:review_list")
+
+    context = {
+        "review": review
+    }
+    return render(request, "reviews/review_confirm_delete.html", context)
