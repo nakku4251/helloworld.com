fe941e8f17138e635bf5eaa9418d6f35ec2e9ca8
柳達也 <nakku@nakkunoMacBook-Air.local>
2021-02-08 11:08:30 +0900

    commit

--- project/urls.py
+++ project/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("diaries/",include("diaries.urls")),
     path("reviews/",include("reviews.urls")),
+    path("contact/",include("contact.urls")),
     path("",include("myprofile.urls")),
 ]
