354f4b064db8bf1227351e38f39a3d3b0109ac65
柳達也 <nakku@nakkunoMacBook-Air.local>
2021-02-19 20:55:18 +0900

    commit

--- project/settings.py
+++ project/settings.py
@@ -129,3 +129,7 @@ EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
 MEDIA_URL = "/media/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static-test"),
+)
